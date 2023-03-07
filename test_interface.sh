91c9ea8cbeb52ab4798fe1cdbadfc9fbd0c3f351
JD Lewis <jlewis@indeed.com>
2023-03-07 10:10:03 -0600

    Fix loop functionality

--- test_interface.sh
+++ test_interface.sh
@@ -8,18 +8,28 @@ TARGET=$1
 shift
 
 if [ -z "$IFACE" ] || [ -z "$COUNT" ] || [ -z "$TARGET" ]; then
-	echo "USAGE: $0 <IFACE> <COUNT> <TARGET_IP>"
+	echo "USAGE: $0 <IFACE> <COUNT> <TARGET_IP> [--loop]"
 	exit 1
 fi
 
 /bin/uname -a | /bin/grep UBNT &>/dev/null && \
-	PING_COMMAND=/usr/bin/sudo /bin/ping -q -c $COUNT -I $IFACE $TARGET || \
-	PING_COMMAND=/bin/ping -q -c $COUNT $TARGET
+	SYSTEM='eos' || \
+	SYSTEM='linux'
+
+ping_func() {
+	if [ $SYSTEM == 'eos' ]; then
+		/usr/bin/sudo /bin/ping -q -c $COUNT -I $IFACE $TARGET
+	else
+		/usr/bin/ping -q -c $COUNT $TARGET
+	fi
+}
 
 if [ "$1" == "--loop" ]; then
 	while true; do
-		`${PING_COMMAND}`
+		ping_func | grep -e 'packets transmitted\|rtt min'
+		echo
+		sleep 0.25
 	done
 else
-	`${PING_COMMAND}`
+	ping_func
 fi
